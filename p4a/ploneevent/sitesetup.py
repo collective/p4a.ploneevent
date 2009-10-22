717ae60fd3bc2e90fcfc1d91b495cb7f604f0119
deo <deo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-22 14:50:17 +0000

    Cleaned up whitespaces. General code review. Removed unused imports.

--- p4a/ploneevent/sitesetup.py
+++ p4a/ploneevent/sitesetup.py
@@ -1,13 +1,7 @@
-from p4a.plonecalendar import content
-from p4a.common import site
-from p4a.z2utils import indexing
-from p4a.z2utils import utils
-from p4a.subtyper.sitesetup import setup_portal as subtyper_setup
-from Products.CMFCore import utils as cmfutils
-
 import logging
-logger = logging.getLogger('p4a.plonecalendar.sitesetup')
+from p4a.common import site
 
+logger = logging.getLogger('p4a.ploneevent.sitesetup')
 
 def setup_portal(portal):
     site.ensure_site(portal)
