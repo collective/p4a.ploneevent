b33ea012996876d8e28e4c73c43d5282a74b8d43
deo <deo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-22 14:57:27 +0000

    Changed schema to use the new field/widget from the archetypes.recurringdate package. Clenaned up code.

--- p4a/ploneevent/recurrence/recurrence.py
+++ p4a/ploneevent/recurrence/recurrence.py
@@ -1,22 +1,15 @@
 import datetime
-from dateutil import rrule, tz
 from zope import interface
 from zope import component
-from zope.app.annotation import interfaces as annointerfaces
-
-from persistent.dict import PersistentDict
-
+from dateable import kalends
 from p4a.ploneevent.recurrence import interfaces
-from p4a.common.descriptors import anno
-from p4a.common.dtutils import DT2dt
-
+from zope.app.annotation import interfaces as annointerfaces
 from Products.ATContentTypes.content.event import ATEvent
 
-from dateable import kalends
 
 class RecurrenceSupport(object):
-    """Recurrence support"""
-
+    """Recurrence support.
+    """
     interface.implements(kalends.IRecurrence)
     component.adapts(ATEvent)
 
@@ -24,82 +17,59 @@ class RecurrenceSupport(object):
         self.context = context
 
     def getRecurrenceRule(self):
-        """Returns a dateutil.rrule"""
-        if getattr(self.context, 'frequency', -1) is -1:
+        """Returns the recurrence rule."""
+        recurrence = getattr(self.context, 'recurrence', None)
+        if not recurrence or not recurrence.enabled:
             return None
-        
-        dtstart = DT2dt(self.context.startDate)
-        if self.context.until is not None:
-            until = DT2dt(self.context.until)
-            until = until.replace(hour=23, minute=59, second=59, microsecond=999999)
-        else:
-            until = None
-        # Make it end at the end of the day:
-
-        rule = rrule.rrule(self.context.frequency,
-                           dtstart=dtstart,
-                           interval=self.context.interval,
-                           #wkst=None, 
-                           count=self.context.count, 
-                           until=until, 
-                           #bysetpos=None,
-                           #bymonth=None, bymonthday=None, byyearday=None, byeaster=None,
-                           #byweekno=None, byweekday=None,
-                           #byhour=None, byminute=None, bysecond=None,
-                           #cache=False
-                       )
-        return rule
+        return recurrence.getRecurrenceRule()
 
     def getOccurrenceDays(self, until=None):
-        """Days on which the event occurs. Used for indexing"""
-        # XXX Handle when there is no occurrence.
+        """Days on which the event occurs. Used for indexing."""
         rule = self.getRecurrenceRule()
         if rule is None:
             return []
+
         if until is None:
             until = datetime.datetime.now() + \
                     datetime.timedelta(365*5)
 
         if until.tzinfo is None and rule._dtstart.tzinfo is not None:
             until = until.replace(tzinfo=rule._dtstart.tzinfo)
-            
+
         if until.tzinfo is not None and rule._dtstart.tzinfo is None:
             until = until.replace(tzinfo=None)
-                            
+
         if rule._until is None or rule._until > until:
             rule._until = until
-        
+
         return [x.date().toordinal() for x in rule][1:]
 
 
 class EventRecurrenceConfig(object):
-    """An IRecurrenceConfig adapter for events.
-    """
-    
+    """An IRecurrenceConfig adapter for events."""
     interface.implements(interfaces.IRecurrenceConfig)
     component.adapts(ATEvent)
 
     def __init__(self, context):
         self.context = context
 
-    def __get_is_recurring(self):
-        return kalends.IRecurringEvent.providedBy(self.context) and \
-               annointerfaces.IAttributeAnnotatable.providedBy(self.context)
-    def __set_is_recurring(self, activated):
-        ifaces = interface.directlyProvidedBy(self.context)
-        if activated:
-            if not kalends.IRecurringEvent.providedBy(self.context):
-                ifaces += kalends.IRecurringEvent
-            if not annointerfaces.IAttributeAnnotatable.providedBy(self.context):
-                ifaces += annointerfaces.IAttributeAnnotatable
-            if getattr(self.context, 'layout', None) is not None:
-                self.context.layout = 'month.html'
-        else:
-            if kalends.IRecurringEvent in ifaces:
-                ifaces -= kalends.IRecurringEvent
-            if getattr(self.context, 'layout', None) is not None:
-                delattr(self.context, 'layout')
-        interface.directlyProvides(self.context, ifaces)
-
-    is_recurring = property(__get_is_recurring,
-                            __set_is_recurring)
+    @apply
+    def is_recurring():
+        def get(self):
+            return kalends.IRecurringEvent.providedBy(self.context) and \
+                   annointerfaces.IAttributeAnnotatable.providedBy(self.context)
+        def set(self, activated):
+            ifaces = interface.directlyProvidedBy(self.context)
+            if activated:
+                if not kalends.IRecurringEvent.providedBy(self.context):
+                    ifaces += kalends.IRecurringEvent
+                if not annointerfaces.IAttributeAnnotatable.providedBy(self.context):
+                    ifaces += annointerfaces.IAttributeAnnotatable
+                if getattr(self.context, 'layout', None) is not None:
+                    self.context.layout = 'month.html'
+            else:
+                if kalends.IRecurringEvent in ifaces:
+                    ifaces -= kalends.IRecurringEvent
+                if getattr(self.context, 'layout', None) is not None:
+                    delattr(self.context, 'layout')
+            interface.directlyProvides(self.context, ifaces)
--- p4a/ploneevent/recurrence/schemaextender.py
+++ p4a/ploneevent/recurrence/schemaextender.py
@@ -1,69 +1,40 @@
-# Trying to use archetypes.schemaextender
+from dateutil import rrule
 from zope import component, interface
-from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
-from archetypes.schemaextender.field import ExtensionField
-from Products.Archetypes import atapi
-from Products.Archetypes.utils import OrderedDict
-from Products.ATContentTypes.content.event import ATEvent
 from dateable.kalends import IRecurringEvent
-from p4a.ploneevent.interfaces import IEventSchemaExtension
-from dateutil.rrule import YEARLY, MONTHLY, WEEKLY, DAILY
+
+from Products.Archetypes import atapi
+from archetypes.recurringdate import field
+from archetypes.recurringdate import RecurringDateWidget
+from archetypes.schemaextender.field import ExtensionField
+from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
+
 from p4a.ploneevent import PloneEventMessageFactory as _
+from p4a.ploneevent.interfaces import IEventSchemaExtension
 
-class TextField(ExtensionField, atapi.TextField):
-     pass
 
-class DateTimeField(ExtensionField, atapi.DateTimeField):
-     pass
+class RecurringDateField(ExtensionField, field.RecurringDateField):
+    pass
 
-class IntegerField(ExtensionField, atapi.IntegerField):
-     pass
-
-class StringField(ExtensionField, atapi.StringField):
-     pass
 
 class RecurrenceExtension(object):
-     component.adapts(IOrderableSchemaExtender, IRecurringEvent)
-     interface.implements(IEventSchemaExtension)
+    component.adapts(IOrderableSchemaExtender, IRecurringEvent)
+    interface.implements(IEventSchemaExtension)
 
-     fields = [
-          IntegerField('frequency',
-               schemata='recurrence',
-               required=True,
-               vocabulary={-1: _(u'Does not repeat'),
-                           YEARLY: _(u'Yearly'),
-                           MONTHLY: _(u'Monthly'),
-                           WEEKLY: _(u'Weekly'),
-                           DAILY: _(u'Daily'),
-                           }.items(),
-               default=-1,
-               widget=atapi.SelectionWidget(label=_(u'Repeats'))
-               ),
-          IntegerField('interval',
-               schemata='recurrence',
-               required=True,
-               default=1,
-               widget=atapi.IntegerWidget(label=_(u'Repeats every'),
-                    description=_(u'Repeats every day/week/month/year.'))
-               ),
-          DateTimeField('until',
-               schemata='recurrence',
-               widget=atapi.CalendarWidget(label=_(u'Range'),
-                    description=_(u'Event repeats until this date.'),
-                    show_hm=True)
-               ),
-          IntegerField('count',
-                schemata='recurrence',
-                widget=atapi.IntegerWidget(label=_(u'Count'),
-                    description=_(u'Maximum number of times the event repeats'),)
-                ),
-          ]
+    fields = [
 
-     def __init__(self, extender, context):
-          pass
+        RecurringDateField(
+            name='recurrence',
+            schemata='recurrence',
+            widget=RecurringDateWidget(),
+        ),
 
-     def getFields(self):
-          return self.fields
-     
-     def getOrders(self):
-          return [(10, 'recurrence')]
+    ]
+
+    def __init__(self, extender, context):
+        pass
+
+    def getFields(self):
+        return self.fields
+
+    def getOrders(self):
+        return [(10, 'recurrence')]
