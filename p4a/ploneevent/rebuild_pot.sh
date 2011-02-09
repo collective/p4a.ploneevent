6302549790d0df6bc79896465c8e4404fdcf2d92
macagua <macagua@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-02-09 19:03:17 +0000

    Updated bash script for generate a log file for error of i18n

--- p4a/ploneevent/rebuild_pot.sh
+++ p4a/ploneevent/rebuild_pot.sh
@@ -10,3 +10,19 @@ find locales -maxdepth 1 -mindepth 1 -type d \
      | grep -v .svn \
      | sed -e "s/locales\/\(.*\)$/\1/" \
      | xargs -I % i18ndude sync --pot locales/$DOMAIN.pot locales/%/LC_MESSAGES/$DOMAIN.po
+
+ERRORS=`find ./ -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR' | wc -l`
+WARNINGS=`find ./ -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
+FATAL=`find ./ -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
+
+echo
+echo "There are $ERRORS errors \(almost definitely missing i18n markup\)"
+echo "There are $WARNINGS warnings \(possibly missing i18n markup\)"
+echo "There are $FATAL fatal errors \(template could not be parsed, eg. if it\'s not html\)"
+echo "For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or" 
+echo "Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir" 
+
+rm ./rebuild_i18n.log
+touch ./rebuild_i18n.log
+
+find ./ -name "*pt" | xargs i18ndude find-untranslated > ./locales/rebuild_i18n.log
